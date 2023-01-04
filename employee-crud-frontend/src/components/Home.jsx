7d8ca5e23a9bcdd01544300f014d12c523bc780e
Nhat Pham <nhat2up@gmail.com>
2023-01-04 09:10:55 +0700

    function delete

--- employee-crud-frontend/src/components/Home.jsx
+++ employee-crud-frontend/src/components/Home.jsx
@@ -1,24 +1,47 @@
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import empService from "../services/emp.service";
 
 export default function Home() {
 
   //function list
   const [empList, setEmpList] = useState([]);
-
-  useEffect(() => {
+  //chuyen thanh 1 ham khac example init khi delete xong thi update lai list
+  const init = () =>{
     empService.getAllEmp().then((res) => {
       console.log(res.data);
       setEmpList(res.data);
     }).catch((error)=>{
       console.log(error);
     })
+  }
+  useEffect(() => {
+    init();
   },[]);
+
+  //message
+  const [msg, setMsg] = useState("")
+  //navigate
+  const navigate = useNavigate();
+  //function delete
+  const deleteEmp = (id) =>{
+    console.log('day la id: ' , id)
+    empService.deleteEmp(id).then((res) => {
+      setMsg("Delete successfully....")
+      init();//update lai list
+    }).catch((error) => {
+      console.log(error)
+    })
+  }
 
   return (
     <div className="container">
       <h1 className="text-center mt-5">Employee System</h1>
+
+      {
+        msg && <p className="text-success text-center">{msg}</p>
+      }
+
       <table className="table mt-5">
         <thead>
           <tr>
@@ -42,7 +65,7 @@ export default function Home() {
               <td>{e.salary}</td>
               <td>
                 <Link className="btn btn-sm btn-primary" to={"editEmployee/" + e.id}>Edit</Link>
-                <Link className="btn btn-sm btn-danger ms-2">Delete</Link>
+                <a onClick={() => deleteEmp(e.id)} className="btn btn-sm btn-danger ms-2">Delete</a>
               </td>
             </tr>
           ))}
